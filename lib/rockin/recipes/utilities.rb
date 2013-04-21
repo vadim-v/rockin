94d1ead7e64dabf3afc97a7870a99ea1d1ccad42
Vadim Vakhovsky <vadim.vakhovsky@gmail.com>
2013-04-21 03:56:10 +0300

    Configuration changes * add "preload_app true" to unicorn config b/c newrelic won't work without it * use sudo to restart unicorn * change a name of a logfile from production.log to unicorn.log in tail_logs capistrano task

--- lib/rockin/recipes/utilities.rb
+++ lib/rockin/recipes/utilities.rb
@@ -2,7 +2,7 @@ Capistrano::Configuration.instance(:must_exist).load do
   
   desc "tail production log files" 
   task :tail_logs, :roles => :app do
-    run "tail -f #{shared_path}/log/production.log" do |channel, stream, data|
+    run "tail -f #{shared_path}/log/unicorn.log" do |channel, stream, data|
       puts  # for an extra line break before the host name
       puts "#{channel[:host]}: #{data}" 
       break if stream == :err    
