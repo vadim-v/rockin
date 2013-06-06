167587e8a96ddb68b4c0da5d0ab93218ae889d47
Vadim Vakhovsky <vadim.vakhovsky@gmail.com>
2013-06-06 22:42:49 +0300

    New recipe: SOLR * postgres: specifying DB encoding upon creation as UTF8

--- lib/rockin/recipes/postgresql.rb
+++ lib/rockin/recipes/postgresql.rb
@@ -10,25 +10,25 @@ Capistrano::Configuration.instance(:must_exist).load do
       run "#{sudo} apt-get -y install postgresql libpq-dev"
     end
     if database.eql?("postgresql")
-      after "deploy:install", "postgresql:install" 
+      after "deploy:install", "postgresql:install"
     end
-    
+
     desc "Create a database for this application."
     task :create_database, roles: :db, only: {primary: true} do
       run %Q{#{sudo} -u postgres psql -c "create user #{postgresql_user} with password '#{postgresql_password}';"}
-      run %Q{#{sudo} -u postgres psql -c "create database #{postgresql_database} owner #{postgresql_user};"}
+      run %Q{#{sudo} -u postgres psql -c "create database #{postgresql_database} owner #{postgresql_user} encoding = 'UTF8';"}
     end
     if database.eql?("postgresql")
-      after "deploy:setup", "postgresql:create_database" 
+      after "deploy:setup", "postgresql:create_database"
     end
-    
+
     desc "Generate the database.yml configuration file."
     task :setup, roles: :app do
       run "mkdir -p #{shared_path}/config"
       template "postgresql.yml.erb", "#{shared_path}/config/database.yml"
     end
     if database.eql?("postgresql")
-      after "deploy:setup", "postgresql:setup" 
+      after "deploy:setup", "postgresql:setup"
     end
 
     desc "Symlink the database.yml file into latest release"
@@ -36,7 +36,7 @@ Capistrano::Configuration.instance(:must_exist).load do
       run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     end
     if database.eql?("postgresql")
-      after "deploy:finalize_update", "postgresql:symlink" 
+      after "deploy:finalize_update", "postgresql:symlink"
     end
   end
 end
